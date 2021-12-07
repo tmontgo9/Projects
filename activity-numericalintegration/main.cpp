b5653f6fa327a5902795f1d6129e7aa294704d8b
Tyler Montgomery <tyler@Tylers-MacBook-Pro.local>
2021-12-06 20:04:34 -0500

    fixed solution

--- activity-numericalintegration/main.cpp
+++ activity-numericalintegration/main.cpp
@@ -2,9 +2,6 @@
 #include <cmath>
 #include <cstdlib>
 #include <chrono>
-#include <ctime>
-#include <time.h>
-
 
 #ifdef __cplusplus
 extern "C" {
@@ -19,50 +16,63 @@ float f4(float x, int intensity);
 }
 #endif
 
-  
+float numericalintegration(int functionid, float a, float b, int n, int intensity)
+{
+  float step1 = (b-a)/n;
+  float area = 0, total = 0;
+  if(functionid ==  1){
+
+  for(int i = 0; i < n; i++){
+      area = step1 * f1(a + (i+.5)*step1, intensity);
+      total = area+total;
+    }
+  }
+  else if(functionid == 2){
+
+  for(int i = 0; i < n; i++){
+      area = step1 *  f2(a + (i+.5)*step1, intensity);
+      total = area+total;
+    }
+  }
+  else if(functionid == 3){
+
+  for(int i = 0; i < n; i++){
+       area = step1 * f3(a + (i+.5)*step1, intensity);
+       total = area+total;
+    }
+  }
+  else if (functionid == 4){
+
+  for(int i = 0; i < n; i++){
+      area = step1 * f4(a + (i+.5)*step1, intensity);
+      total = area+total;
+    }
+  }
+  else{
+    return 1.0;
+  }
+  return total;
+}
 int main (int argc, char* argv[]) {
-    struct timespec start, finish;
-    clock_gettime(CLOCK_MONOTONIC, &start);
-  if (argc < 6) {
+  using namespace std::chrono;
+ if (argc < 6) {
     std::cerr<<"usage: "<<argv[0]<<" <functionid> <a> <b> <n> <intensity>"<<std::endl;
     return -1;
   }
-    int functionid = 0, n = 0;
-      
-      float a = 0, b = 0, intensity = 0;
-      float sum = 0, elapsed = 0;
-      
-      functionid = std::atoi(argv[1]);
-      a = std::atoi(argv[2]);
-      b = std::atoi(argv[3]);
-      n = std::atoi(argv[4]);
-      intensity = std::atoi(argv[5]);
+ double integral = 0;
+ 
+  int functionid = atoi(argv[1]);
+  float a = atof(argv[2]);
+  float b = atof(argv[3]);
+  int n = atoi(argv[4]);
+  int intensity = atoi(argv[5]);
 
-      switch(functionid) {
-          case 1:
-            for(int i = 0; i < n; i++) {
-              sum += f1(a + (i + 0.5)*((b-a) /n), intensity)*((b-a) /n);
-            }
-            break;
-          case 2:
-            for(int i = 0; i < n; i++) {
-              sum += f2(a + (i + 0.5)*((b-a) /n), intensity)*((b-a) /n);
-            }
-            break;
-          case 3:
-            for(int i = 0; i < n; i++) {
-              sum += f3(a + (i + 0.5)*((b-a) /n), intensity)*((b-a) /n);
-            }
-            break;
-          case 4:
-            for(int i = 0; i < n; i++) {
-              sum += f4(a + (i + 0.5)*((b-a) /n), intensity)*((b-a) /n);
-            }
-            break;
-      }
-      
-      std::cout << sum << std::endl;
-      clock_gettime(CLOCK_MONOTONIC, &finish);
-      elapsed = (finish.tv_nsec - start.tv_nsec);
-      return 0;
-    }
+  auto start = std::chrono::steady_clock::now();
+  integral = numericalintegration(functionid, a, b, n, intensity);
+  auto end = std::chrono::steady_clock::now();
+  auto microseconds = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+  float time = microseconds/1000000.0;
+  std::cout << integral << std::endl;
+  std::cerr << std::to_string(time) << " seconds." << std::endl;
+  return 1;
+}
