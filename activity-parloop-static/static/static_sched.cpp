ad738f7fe1a5adc3b7e508154a64d1526cce7261
tmontgo9 <46718227+tmontgo9@users.noreply.github.com>
2021-12-07 19:31:55 -0500

    fixed push issue

--- activity-parloop-static/static/static_sched.cpp
+++ activity-parloop-static/static/static_sched.cpp
@@ -6,6 +6,7 @@
 #include <cmath>
 #include <vector>
 #include <iostream>
+#include "seq_loop.hpp"
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -23,17 +24,20 @@ float f4(float x, int intensity);
 
 int main (int argc, char* argv[]) {
 
+
   auto fptr1 = f1;
   auto fptr2 = f2;
   auto fptr3 = f3;
   auto fptr4 = f4;
 
   std::vector<float (*)(float, int)> funct_list = {fptr1, fptr2, fptr3, fptr4};
+
   
   if (argc < 7) {
     std::cerr<<"usage: "<<argv[0]<<" <functionid> <a> <b> <n> <intensity> <nbthreads>"<<std::endl;
     return -1;
   }
+
   int functionId = atoi(argv[1]);
   float a = atof(argv[2]);
   float b = atof(argv[3]);
@@ -44,6 +48,7 @@ int main (int argc, char* argv[]) {
   auto start =std::chrono::steady_clock::now();
   
   float sum = 0.0;
+
   sl.parfor<float>(1, nbthreads, n,
             [&](float& tls) {
               tls = 0.0;
@@ -51,16 +56,20 @@ int main (int argc, char* argv[]) {
             [&](int i, float& tls) {
               float x = (a + ((float)i + 0.5)) * ((float)(b-a)/n);
               tls += (float)funct_list[functionId - 1](x , intensity);
+               
             },
             [&](float& tls) {
               sum += tls;
+              
             }
         );
-   float answer = ((float)(b-a)/n) *sum;
+
+   float answer = ((float)(b-a)/n) * sum;
+
   auto stop = std::chrono::steady_clock::now();
   std::chrono::duration<double> time_elapsed = stop-start;
 
-  std::cout<<answer<<" \n";
+  std::cout<<answer<<"\n";
   std::cerr << time_elapsed.count();
   return 0;
 }
