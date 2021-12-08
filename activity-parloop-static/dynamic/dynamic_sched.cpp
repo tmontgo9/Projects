f87f8ee91b668a548c3e3ad5f74ce11e44c916ee
tmontgo9 <46718227+tmontgo9@users.noreply.github.com>
2021-12-08 12:32:32 -0500

    git push

--- activity-parloop-static/dynamic/dynamic_sched.cpp
+++ activity-parloop-static/dynamic/dynamic_sched.cpp
@@ -19,17 +19,21 @@ float f4(float x, int intensity);
 }
 #endif
 
-float charToFloat(char* str){
-  char *remaining;
+float charConversion(char* str){
+  char * remaining;
   float number = strtof(str, &remaining);
+  if (str == remaining){
+    std::cerr<<"error"<<std::endl;
+    exit(0);
+  }
   return number;
 }
 
-float Integral(float functionId, float a, float b, int n, int intensity, int nbthreads, int granularity){
+float integralConversion(float functionId, float a, float b, int n, int intensity, int nbthreads, int granularity){
   SeqLoop sl(n, nbthreads, granularity);
-  float eq1 = (float)(b-a)/n;
-  float eqans = 0;
-  std::vector<float> eqansv;
+
+  float eq1 = (float)(b-a)/n; float funcAns = 0;
+  std::vector<float> functionResponseVec;
   sl.parfor<float>(0, n, 1,
     [&](float& tls) -> void{
            tls = 0;
@@ -49,37 +53,39 @@ float Integral(float functionId, float a, float b, int n, int intensity, int nbt
         case 4:
           tls += f4(eq2,intensity);
           break;
+        default:
+          std::cerr<<"error"<<std::endl;
+          exit(0);
         }
           },
          [&](float &tls) -> void{
-      eqans += tls;
+      funcAns += tls;
          }
   );
   
-  return (eq1*eqans);
+  return (eq1*funcAns);
 }
-int main (int argc, char* argv[]) {
+
+int main (int argc, char* argv[]){
 
   if (argc < 8) {
     std::cerr<<"usage: "<<argv[0]<<" <functionid> <a> <b> <n> <intensity> <nbthreads> <granularity>"<<std::endl;
     return -1;
   }
-  float functionId = charToFloat(argv[1]);;
-  float a = charToFloat(argv[2]);
-  float b = charToFloat(argv[3]);
-  int n = charToFloat(argv[4]);
-  int intensity = charToFloat(argv[5]);
-  int nbthreads = charToFloat(argv[6]);
-  int granularity = charToFloat(argv[7]);
+  float functionId = charConversion(argv[1]);
+  float a = charConversion(argv[2]);
+  float b = charConversion(argv[3]);
+  int n = charConversion(argv[4]);
+  int intensity = charConversion(argv[5]);
+  int nbthreads = charConversion(argv[6]);
+  int granularity = charConversion(argv[7]);
   auto start =std::chrono::steady_clock::now();
-
-  float response = (float) Integral(functionId, a, b, n, intensity, nbthreads, granularity);
+  float response = (float) integralConversion(functionId, a, b, n, intensity, nbthreads, granularity);
+  auto stop = std::chrono::steady_clock::now();
   
-    auto stop = std::chrono::steady_clock::now();
-    
-  std::chrono::duration<double> difference = stop-start;
-  std::cout<< response<<std::endl;
-  std::cerr << difference.count() << std::endl;
+    std::chrono::duration<double> difference = stop-start;
+    std::cout<< response<<std::endl;
+    std::cerr << difference.count() << std::endl;
 
   return 0;
 }
