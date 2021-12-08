b9864d0ae9f25c6e9b1b85d7f169c88cb8bd5a4c
tmontgo9 <46718227+tmontgo9@users.noreply.github.com>
2021-12-08 11:19:48 -0500

    final push

--- activity-parloop-static/sequential/seq_loop.hpp
+++ activity-parloop-static/sequential/seq_loop.hpp
@@ -2,9 +2,33 @@
 #define __SEQ_LOOP_H
 
 #include <functional>
+#include <vector>
+#include <thread>
+#include <mutex>
+
+
 
 class SeqLoop {
+
+  int threads, granularity;
+  bool dynamic;
 public:
+  SeqLoop()
+    :threads(1), granularity(1), dynamic(false)
+  {}
+
+  void setNBThread(int i){
+    threads = i;
+  }
+
+  void setGranularity(int i) {
+    granularity = i;
+  }
+
+  void setDynamic(bool b) {
+    dynamic = b;
+  }
+
   /// @brief execute the function f multiple times with different
   /// parameters possibly in parallel
   ///
@@ -12,7 +36,7 @@ public:
   /// beg, no greater than end, in inc increment. These execution may
   /// be in parallel
   void parfor (size_t beg, size_t end, size_t inc,
-	       std::function<void(int)> f) {
+           std::function<void(int)> f) {
     for (size_t i=beg; i<end; i+= inc) {
       f(i);
     }
@@ -35,18 +59,80 @@ public:
   ///
   /// Once the iterations are complete, each thread will execute after
   /// on the TLS object. No two thread can execute after at the same time.
+
   template<typename TLS>
   void parfor (size_t beg, size_t end, size_t increment,
-	       std::function<void(TLS&)> before,
-	       std::function<void(int, TLS&)> f,
-	       std::function<void(TLS&)> after
-	       ) {
-    TLS tls;
-    before(tls);    
-    for (size_t i=beg; i<end; i+= increment) {
-      f(i, tls);
+           std::function<void(TLS&)> before,
+           std::function<void(int, TLS&)> f,
+           std::function<void(TLS&)> after
+           ) {
+    TLS* tls = new TLS[threads];
+    
+    std::vector<std::thread> threadpool;
+    
+    // before
+    for (int i = 0; i < threads;i++)
+      before(tls[i]);
+
+    
+    // dynamic
+    if (dynamic) {
+      
+      int point = beg;
+      std::mutex mu;
+
+      for (size_t i = 0; i<threads; i++) {
+        threadpool.push_back(std::thread(
+        [&, i](){
+
+          mu.lock();
+          int local_point = point;
+          point += granularity;
+          mu.unlock();
+
+          while (local_point < end) {
+            for (int j = local_point; j < (local_point + granularity) && j < end ;j+=increment)
+              f(j, tls[i]);
+
+            mu.lock();
+            local_point = point;
+            point += granularity;
+            mu.unlock();
+          }
+
+        }));
+      }
+      
     }
-    after(tls);
+
+    // static
+    else {
+      // determine static spacing
+      std::vector<int> spacing;
+      int gap = (end - beg) / threads;
+
+      for(int i = beg; i < end;) {
+        spacing.push_back(i);
+        i += gap;
+        if ((end - i) % threads != 0) i++;
+      }
+      spacing.push_back(end);
+    
+      // run loop
+      for (size_t i = 0; i<threads; i++) {
+        threadpool.push_back(std::thread(
+        [&, i](){
+          for (int j = spacing[i]; j < spacing[i+1]; j++)
+            f(j, tls[i]);
+        }));
+      }
+    }
+    
+    for (int i = 0; i < threads; i++){
+      threadpool[i].join();
+      after(tls[i]);
+    }
+    
   }
   
 };
