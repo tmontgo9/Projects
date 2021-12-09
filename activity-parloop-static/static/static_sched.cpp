7bdb2f9dc10bdf51a623d50ad247668a4e4d856f
tmontgo9 <46718227+tmontgo9@users.noreply.github.com>
2021-12-09 18:45:31 -0500

    test

--- activity-parloop-static/static/static_sched.cpp
+++ activity-parloop-static/static/static_sched.cpp
@@ -2,16 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <chrono>
-#include <cmath>
-
+#include <thread>
 #include <vector>
+#include <array>
 #include "../sequential/seq_loop.hpp"
-
 #ifdef __cplusplus
 extern "C" {
 #endif
-
+//simplified, fix later
 float f1(float x, int intensity);
 float f2(float x, int intensity);
 float f3(float x, int intensity);
@@ -20,64 +18,59 @@ float f4(float x, int intensity);
 #ifdef __cplusplus
 }
 #endif
-
-float integral ( float functionid, float a, float b, int n, int intensity, int nbthreads)
-{
-  SeqLoop s(n, nbthreads);
-  float diff=(b-a);
-  float c=diff/n;
-  float f=0;
-  std::vector <float> fvec;
-  
-  s.parfor<float>(0,
-           n,
-           1,
-           [&](float& function) -> void
-           {
-             function = 0;
-           },
-           [&] (int i, float& function) -> void
-           {
-             float integral =(a+((i+0.5)*c));
-                if(functionid==1)
-                        function=function+f1(integral,intensity);
-                else if(functionid==2)
-                        function=function+f2(integral,intensity);
-                else if(functionid==3)
-                  function=function+f3(integral,intensity);
-                else if(functionid==4)
-                        function=function+f4(integral,intensity);
-           },
-          [&] (float &function) -> void
-          {
-            f=f+function;
-          }
-          );
-
-           return (f * c);
+typedef float (*ptr) (float,int);
+ptr getFunction(int f) {
+  switch (f) {
+    case 1:
+      return &f1;
+    case 2:
+      return &f2;
+    case 3:
+      return &f3;
+    default:
+      return &f4;
+  }
 }
-int main (int argc, char* argv[]){
+int main (int argc, char* argv[]) {
 
-  if (argc < 7)
-  {
+  auto start = std::chrono::system_clock::now();
+  if (argc < 7) {
     std::cerr<<"usage: "<<argv[0]<<" <functionid> <a> <b> <n> <intensity> <nbthreads>"<<std::endl;
     return -1;
   }
-  float functionid=atoi(argv[1]);
-  float a=strtof(argv[2],NULL);
-  float b=strtof(argv[3],NULL);
-  int n=atoi(argv[4]);
-  int intensity=atoi(argv[5]);
-  int nbthreads = atoi(argv[6]);
+  std::array<float, 6> point;
+  SeqLoop s1;
+  for (int i = 0; i < 6; i++) {
+    point[i] = atoi(argv[i + 1]);
+  }
 
-    auto starttime=std::chrono::system_clock::now();
-    float function = (float) integral( functionid,a,b,n,intensity,nbthreads);
+  float ans = 0;
+  int func = point[0];
+  int a = point[1];
+  int b = point[2];
+  int n = point[3];
+  int intensity = point[4];
+  int threads = point[5];
 
-        auto endtime=std::chrono::system_clock::now();
-        std::chrono::duration<double> timetaken=endtime-starttime;
-    
-        std::cout<<function<<"\n";
-        std::cerr<<timetaken.count()<<"\n";
+  float (*ptr)(float, int) = getFunction(func);
+  float t1 =  (b - a) / float (n);
+    s1.setNBThread(threads);
+    s1.parfor<float>(
+    0, n, 1,
+    [&](float (&tls)) -> void { tls =0;
+    },
+    [&](int i, float (&tls)) -> void { tls += (*ptr)(a + ((i +.5) * t1), intensity);
+    },
+    [&](float (&tls)) -> void {
+      ans +=B;
+    }
+  );
   
+  ans = ans *t1;
+  auto end = std::chrono::system_clock::now();
+  std::chrono::duration<double> diff =end -start;
+  std::cout << ans << std::endl;
+  std::cerr << diff.count() << std::endl;
+
   return 0;
 }
