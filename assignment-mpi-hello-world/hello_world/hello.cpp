5f08aa6f5be6717ae43f1f24d123a12b3ede3dd6
tmontgo9 <46718227+tmontgo9@users.noreply.github.com>
2021-12-09 21:30:39 -0500

    finished mpi hello

--- assignment-mpi-hello-world/hello_world/hello.cpp
+++ assignment-mpi-hello-world/hello_world/hello.cpp
@@ -1,21 +1,16 @@
 #include <mpi.h>
 #include <unistd.h>
-#include <limits.h>
 #include <iostream>
-
+#include <limits.h>
 int main(int argc, char*argv[]) {
 
     MPI_Init(&argc, &argv);
     int size, rank;
-    
     char hostname[HOST_NAME_MAX];
     gethostname(hostname, HOST_NAME_MAX);
-
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    std::cout << "I am process " << rank
-        << " out of " << size
-        << ". I am running on " << hostname << std::endl;
+    std::cout << "I am process " << rank << " out of " << size << ". I am running on: " << hostname << std::endl;
     MPI_Finalize();
 }
